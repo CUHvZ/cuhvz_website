d45d7957a97c485e5c01b86474971f588128bda5
Josh <jobr3255@colorado.edu>
2018-12-16 08:20:55 -0700

    fixed footer

--- layout/footer.php
+++ layout/footer.php
@@ -3,26 +3,26 @@
 
 <div class="section footer">
   <div class="footer-container">
-    <div class="container footer-box">
-      Contact us with any questions <br/>
+    <span class="container footer-box">
+      Any questions?<br/>
       <a class="button button-primary" href="mailto:humansvszombies@Colorado.edu">Email Us</a>
-    </div>
-    <div class="container footer-box">
+    </span>
+    <span class="container footer-box">
       Join our discord! <br/>
       <a class="button button-primary" href="https://discord.gg/nzqG7Wk">Discord</a>
-    </div>
-    <div class="container footer-box">
+    </span>
+    <span class="container footer-box">
       Follow us <br/>
       <a class="button button-primary" href="http://www.facebook.com/HvZatCU/" target="_blank">Facebook</a>
-    </div>
-    <div class="container footer-box">
-      Want to join the team? <br/>
+    </span>
+    <span class="container footer-box">
+      Join our team! <br/>
       <a class="button button-primary" href="https://docs.google.com/forms/d/e/1FAIpQLSeCbxWdl3ozaa_9fqwBj_W7CT-RRn2lHCZfvQvCITCujijF7g/viewform?usp=sf_link" target="_blank">Apply here!</a>
-    </div>
-    <div class="container footer-box">
+    </span>
+    <span class="container footer-box">
       Enjoy our events? <br/>
       <a class="button button-primary" href="https://www.eventbrite.com/e/cu-hvz-2018-fall-donations-tickets-52765200134" target="_blank">Donate now!</a>
-    </div>
+    </span>
   </div>
 </div> <!-- end footer section -->
 
