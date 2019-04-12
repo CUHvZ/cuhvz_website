f543780ca07e8c310e303b12eb710723bd796a21
Josh <jobr3255@colorado.edu>
2019-04-12 09:43:19 -0600

    added ragnarok lockin eventbrite

--- lockin/details/spring19/details.php
+++ lockin/details/spring19/details.php
@@ -1,7 +1,25 @@
-Don't forget to sign and bring your wavier! <br/>
-<a href="/lockin/waivers/lockin_waiver_spring19.pdf" target="_blank" >Ragnarok Lock-In waiver</a>
+<div>
+  <strong>Important note to all player under the age of 18:</strong>
+  <br/>
+  Due to rules placed on us that are out of our control we require a parent/guardian to be present to sign your wavier in order to prove that a signature was not forged.
+  If your parent cannot be present to sign your wavier you will not be allowed to participate in the lock-in.
+</div>
 <br/>
-<strong>Important note to all player under the age of 18:</strong>
-<br>
-Due to rules placed on us that are out of our control we require a parent/guardian to be present to sign your wavier in order to prove that a signature was not forged.
-If your parent cannot be present to sign your wavier you will not be allowed to participate in the lock-in.
+<div>
+  Don't forget to sign and bring your wavier! <br/>
+  <a href="/lockin/waivers/lockin_waiver_spring19.pdf" target="_blank" >Ragnarok Lock-In waiver</a>
+</div>
+<br/>
+<div>
+  <h3>
+    <a href="https://www.eventbrite.com/e/cu-hvz-ragnarok-tickets-60309888500" target="_blank">Get you ticket here!!</a>
+  </h3>
+</div>
+<br/>
+<div>
+  Don't have a Nerf blaster? No problem, you can rent one!
+  <br/>
+  <h5>
+    <a href="https://www.eventbrite.com/e/cu-hvz-ragnarok-blaster-rental-tickets-60286249796" target="_blank">Rent a blaster</a>
+  </h5>
+</div>
