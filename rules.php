c398307ac0ac6557c796eae1aba33fbf6d2fafe4
Josh Brown <jobr3255@colorado.edu>
2019-11-09 15:29:33 -0700

    Updated rules

--- rules.php
+++ rules.php
@@ -15,50 +15,75 @@ $title = 'CU HvZ | Rules';
 	<div class="container">
 		<div class="row">
 		<div class="content lightslide-box white">
-			<h2><strong>Week Long</strong></h2>
-			<p>
+			<h2><strong>Weeklong</strong></h2>
+			<div>
 				<h5>1. Don’t be a douchebag</h5>
-				- Don’t use non-players as human shields <br/>
-				- Don’t remove your bandana to avoid being killed <br/>
-				- Don’t deny that you weren’t tagged/stunned <br/>
-				- Don’t make mountains out of mole hills, this is a game, not life or death <br/>
-			</p>
-			<p>
-				<h5>2. Wear your bandana when you are playing</h5>
-				- Humans wear bandanas on their arms, zombies wear bandanas around their heads
-				<div>
-					<img src="images/bandana_arm.png" style="width: 30%">
-					<img src="images/bandana_head.png" style="width: 50%">
-				</div>
-			</p>
-			<p>
-				<h5>Don’t involve non players in the game</h5>
-				- If someone isn’t wearing a bandana (bright orange and provided by us) don’t try to involve them in the game in anyway <br/>
-				- We can only host these games if the whole CU community feels safe <br/>
-			</p>
-			<p>
-				<h5>4. Only play within the game limits</h5>
-				- All buildings are off-limits <br/>
-				- Gameplay will only be on campus and at designated parks around Boulder <br/>
-				<div><img src="images/play_area.png" style="width: 50%"></div>
-			</p>
-			<p>
-				<h5>5. <strong>No nerf on campus!</strong></h5>
-				- We wish we could use nerf too but unfortunately no nerf is allowed on campus at all!!! <br/>
-				- You can protect yourself with rolled up socks, blowguns (must be shorter than 3 feet) or thrown darts and other soft projectiles. <br/>
-			</p>
-			<p>
-				<h5>6. Only play within game times, Monday-Friday 8:00-5:00</h5>
-				- There will also be an off-campus mission at 5 in a park in Boulder. <br/>
-				- During these mission Nerf is allowed but only at these off-campus missions. <br/>
-			</p>
-			<p>
-				<h5>7. Always have your player code on you to give to a zombie when you get tagged</h5>
-				- Have your code on a notecard, a screenshot on your phone, any way you can quickly access it is fine. <br/>
-			</p>
-		</div> <!-- end row -->
-	</div> <!-- end container -->
-</div> <!-- end signup section -->
+				<ul class="plain">
+					<li class="plain">Don’t use non-players as human shields</li>
+					<li class="plain">Don’t remove your bandana to avoid being killed</li>
+					<li class="plain">Don’t deny that you weren’t tagged/stunned</li>
+					<li class="plain">Don’t make mountains out of molehills, this is a game, not life or death</li>
+				</ul>
+			</div>
+			<div>
+				<h5>2. <strong>No nerf on campus!</strong></h5>
+				<ul class="plain">
+					<li class="plain">We wish we could use nerf too, but unfortunately no nerf is allowed on campus at all!!!</li>
+					<li class="plain">You can protect yourself with rolled up socks (preferably clean thanks) and other soft similarly-sized projectiles</li>
+				</ul>
+			</div>
+			<div>
+				<h5>3. Wear your bandanas when you are playing</h5>
+				<ul class="plain">
+					<li class="plain">All participants wear bandanas around their ankles to designate that they are a player</li>
+					<li class="plain">Humans wear bandanas on their arms</li>
+					<li class="plain">Zombies wear bandanas around their heads</li>
+				</ul>
+				<img src="images/human.jpg" style="width: 40%">
+				<img src="images/zombie.jpg" style="width: 40%">
+			</div>
+			<div>
+				<h5>4. Don’t involve non players in the game</h5>
+				<ul class="plain">
+					<li class="plain">If someone isn’t wearing two Mod-provided bandanas (one bright yellow on the ankle and one bright green on the head or arm) don’t try to involve them in the game in any way</li>
+					<li class="plain">We can only host these games if the whole CU community feels safe</li>
+				</ul>
+			</div>
+			<div>
+				<h5>5. Only play within the game limits</h5>
+				<ul class="plain">
+					<li class="plain">All buildings are off-limits</li>
+					<li class="plain">No gameplay on anything with wheels (includes bus, bike, Heelies, etc)</li>
+					<li class="plain">Gameplay will only be on Main campus and at designated parks around Boulder</li>
+				</ul>
+				<img src="images/play_area.png" style="width: 50%">
+			</div>
+			<div>
+				<h5>6. Only play within game times</h5>
+				<ul class="plain">
+					<li class="plain">On-campus game times are Monday-Friday 9:00am - 5:00pm</li>
+					<li class="plain">There will also be off-campus missions at local parks in Boulder: details will be provided in daily emails and on the website including starting times and locations</li>
+					<li class="plain">Starting times are typically betwen 5 and 6pm and take around 30 minutes depending on participation</li>
+					<li class="plain">During these off-campus mission Nerf is allowed</li>
+				</ul>
+			</div>
+			<div>
+				<h5>7. Player codes</h5>
+				<ul class="plain">
+					<li class="plain">Always have your player code on you to give to a zombie when you get tagged</li>
+					<li class="plain">Have your code on a notecard, a screenshot on your phone--any way you can quickly access it is fine</li>
+					<li class="plain">You can find your player code on your profile page</li>
+				</ul>
+			</div>
+			<div>
+				<h5>Other Notes:</h5>
+				<ul class="plain">
+					<li class="plain">All players must sign up on the website and fill out a safety waiver before playing</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
 
 <!-- End Document
 –––––––––––––––––––––––––––––––––––––––––––––––––– -->
