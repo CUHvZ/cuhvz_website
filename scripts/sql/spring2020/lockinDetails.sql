b2518cbae2a7160bf5f36fed04273027128921c7
Josh Brown <jobr3255@colorado.edu>
2020-03-02 14:37:02 -0700

    Lockin details schedule

---
+++ scripts/sql/spring2020/lockinDetails.sql
@@ -0,0 +1,33 @@
+update lockins set details=
+'Years after the return of the Avatar and the war had ended a new nation emerged, a nation of blood. Powered by dark spirits and fueled by rage and revenge for their people, a group of blood benders formed Blood Nation.
+
+The blood benders made a deal with the dark spirits, allowing the spirits to inhabit their bodies in exchange for enhancing their blood bending capabilities beyond imagination. The dark spirits bestowed the ability to control anyone from anywhere as long as they have been infected with dark spirit energy.
+
+They have declared war on the Fire Nation and have begun to advance their brainwashed armies. Coming into contact with anyone infected with the dark spirit energy results in the infection immediatelly spreading and taking control of their mind, joining the Blood Nation.
+
+BOLD[Important note to all player under the age of 18:]
+Due to rules placed on us that are out of our control we require a parent/guardian to be present to sign your wavier in order to prove that a signature was not forged.
+If your parent cannot be present to sign your wavier you will not be allowed to participate in the lock-in.
+
+BOLD[Tentative schedule:]
+BOLD[Please not that there is roughly 2.5 - 3 hours between doors opening and when gameplay starts.]
+9:00 pm: Doors open and check-in begins
+9:15 pm: Rules presentation
+9:30 pm: Rules presentation
+9:45 pm: Rules presentation
+10:00 pm: Doors close
+10:15 pm: Final rules presentation
+10:30 - 11:00 pm: First round begins
+(Note: This when we aim to start the first round and is subject to change)
+~12:00 pm First round ends
+Next 15-30 minutes is dedicated to clean up and next round setup
+~12:30 pm Second round begins
+~ 1:30: Second round ends
+The rest of the time is dedicated to cleaning up
+
+
+
+Parking:
+IMAGE[/images/where-to-park.png]
+'
+where id=5;
--- scripts/sql/spring2020/weeklongDetails.sql
+++ scripts/sql/spring2020/weeklongDetails.sql
@@ -3,9 +3,17 @@ update weeklong_details set details=
 
 The blood benders made a deal with the dark spirits, allowing the spirits to inhabit their bodies in exchange for enhancing their blood bending capabilities beyond imagination. The dark spirits bestowed the ability to control anyone from anywhere as long as they have been infected with dark spirit energy.
 
-They have declared war on the Fire Nation and have begun to advance their brainwashed armies. Coming into contact with anyone infected with the dark spirit energy results in the infection immediatelly spreading and taking control of their mind, joining the Bood Nation.
+They have declared war on the Fire Nation and have begun to advance their brainwashed armies. Coming into contact with anyone infected with the dark spirit energy results in the infection immediatelly spreading and taking control of their mind, joining the Blood Nation.
 
-Details/Rules:
+BOLD[When to turn in your waiver and get your bandanas:]
+We will be tabling in the UMC during these times
+Tuesday 3/3, 9 am - 5pm
+Wednesday 3/4, 9 am - 5pm:
+Thursday 3/5, 9 am - 5pm
+Friday 3/6, 9 am - 5pm
+Monday 3/9, 9 am - 5pm
+
+BOLD[Details/Rules:]
 - Off-campus is not perma death for humans, just for fun and points. Humans that get tagged during off-campus missions will remain human after the mission and zombies will still get their points.
 - Yellow bandana must be worn around your leg to identify you as a player of the game.
 - The green bandana is worn on your head for zombies and around your arm for humans.
@@ -16,16 +24,3 @@ Suppy Drop Locations:
 IMAGE[/images/signs-fall19.png]
 '
 where weeklong_id=6;
-
-update lockins set details=
-'Years after the return of the Avatar and the war had ended a new nation emerged, a nation of blood. Powered by dark spirits and fueled by rage and revenge for their people, a group of blood benders formed Blood Nation.
-
-The blood benders made a deal with the dark spirits, allowing the spirits to inhabit their bodies in exchange for enhancing their blood bending capabilities beyond imagination. The dark spirits bestowed the ability to control anyone from anywhere as long as they have been infected with dark spirit energy.
-
-They have declared war on the Fire Nation and have begun to advance their brainwashed armies. Coming into contact with anyone infected with the dark spirit energy results in the infection immediatelly spreading and taking control of their mind, joining the Bood Nation.
-
-BOLD[Important note to all player under the age of 18:]
-Due to rules placed on us that are out of our control we require a parent/guardian to be present to sign your wavier in order to prove that a signature was not forged.
-If your parent cannot be present to sign your wavier you will not be allowed to participate in the lock-in.
-'
-where lockins=5;
