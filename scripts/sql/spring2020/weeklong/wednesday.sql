03202fcf7a5f610aa314a9c754558262e65da2a9
Josh Brown <jobr3255@colorado.edu>
2020-03-11 12:30:55 -0600

    update wednesday

--- scripts/sql/spring2020/weeklong/wednesday.sql
+++ scripts/sql/spring2020/weeklong/wednesday.sql
@@ -9,10 +9,37 @@ While you’re in the Earth Kingdom we’d like you to recruit Toph Beifong to a
 
 - General Zuko
 
+[LINE]
+
+[SUPPLY_DROPS]
+One of these has been poisoned
+Sign #4: 10 supplies
+Sign #7: 10 supplies
+Sign #11: 10 supplies
+Sign #19: 10 supplies
+Poisoned supply drops will reduce the effectiveness of the next 3 supply drops used. After 3 unpoisoned supply drops have been collected the poison is cured.
+LINK_NEW_TAB[Sign Location Map][/images/signs-fall19.png]
+
 [ON_CAMPUS]
 Mission: Selfies
 Time: 9 am - 5 pm
 Location: Find them
+
+Total of 16 selfies must be submitted in order to complete the mission
+Mission failure: Zombie stun timer reduced by 1 minute
+Mission Rewards:
+1/4 correct selfies = 5 points
+2/4 correct selfies = 10 points
+3/4 correct selfies = 20 points
+4/4 correct selfies = 40 points
+Best voted selfie (with permission from the player) will receive 10 bonus points.
+How to submit selfies:
+- All selfies must be submitted at the same time along with your username and player code through Discord photo-submissions channel.
+- Selfies must be submitted to the photo-submissions chat on discord by 5 pm.
+- Player must be clearly visible and recognizable in the picture. Group pictures are fine as long as we can identify the player submitting the photos, so circle yourself in one of the selfies so we know it\'s you.
+- Submissions that are missing the username and player code will not be counted
+IMAGE[/images/selfies-spring2020.jpg]
+
 
 [OFF_CAMPUS]
 Mission: King of the Hill
