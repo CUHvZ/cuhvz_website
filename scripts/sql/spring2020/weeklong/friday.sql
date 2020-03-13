7a1e0dd15b41db7f927fa0a65d6eb4bf179a4406
Josh Brown <jobr3255@colorado.edu>
2020-03-13 10:35:49 -0600

    friday updates

--- scripts/sql/spring2020/weeklong/friday.sql
+++ scripts/sql/spring2020/weeklong/friday.sql
@@ -10,13 +10,10 @@ Our last hope is the Avatar. Avatar Aang will travel to the Spirit world to talk
 - General Zuko
 
 [ON_CAMPUS]
-Mission: Sign the Notebook
-Time: 9 am - 5 pm
-Location: LINK_NEW_TAB[Rock with square hole][https://www.google.com/maps/@40.0079274,-105.2711151,3a,75y,35.01h,74.28t/data=!3m6!1e1!3m4!1sf2hKqkG6UyTgThoKv6XnOQ!2e0!7i13312!8i6656]
+Canceled
 
 [OFF_CAMPUS]
-Mission: Mobile defense
-Location: LINK_NEW_TAB[Martin park][https://www.google.com/maps/place/Martin+Park/@39.9889483,-105.2520282,17z/data=!3m1!4b1!4m12!1m6!3m5!1s0x876beda6c350f9cd:0xbfe19b6c4417032e!2sMartin+Park!8m2!3d39.9889442!4d-105.2498342!3m4!1s0x876beda6c350f9cd:0xbfe19b6c4417032e!8m2!3d39.9889442!4d-105.2498342]
-Start Time: 5:30 pm
-All players present receive 10 points and +6 hours on their starve timer'
+Canceled
+
+Due to the rising risks of Covid-19 we have decided to cancel Friday\'s on and off-campus missions. Stay safe out there.'
 where weeklong_id=6;
